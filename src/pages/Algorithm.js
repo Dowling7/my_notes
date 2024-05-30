f49cd3d4b484af87871a6d17b62826a65488030b
Dowling_Wong <80257366+Dowling7@users.noreply.github.com>
2024-05-30 14:05:19 -0400

    deploy, added code

--- src/pages/Algorithm.js
+++ src/pages/Algorithm.js
@@ -1,10 +1,38 @@
 import React from "react";
 import Layout from "../Layout";
+import 'katex/dist/katex.min.css';
+import { InlineMath, BlockMath } from 'react-katex';
+import 'prismjs/themes/prism-okaidia.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 function Algorithm() {
   return (
-    
-    <div>This is the Algorithm page.</div>
+    <main>
+      <hr />
+      <h2>Kmeans implementation at SKlearn</h2>
+      <BlockMath math="c = \\pm\\sqrt{a^2 + b^2}" />
+      <SyntaxHighlighter language="python">
+        {`import numpy as np
+          from sklearn.cluster import KMeans
+          X = np.array([[1, 2], [1, 4], [1, 0],
+                        [10, 2], [10, 4], [10, 0]])
+          kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+          print(kmeans.labels_)
+          `}
+      </SyntaxHighlighter>
+      <hr />
+      <h2>Silicon Modules at CMS</h2>
+      <hr />
+      <h2>DarkQuest</h2>
+      <SyntaxHighlighter language="cpp">
+        {`#include <iostream>
+          int main() {
+            std::cout << "Hello, World!";
+            return 0;
+          }
+          `}
+      </SyntaxHighlighter>
+    </main>
   );
 }
 
--- src/pages/MachineLearning.js
+++ src/pages/MachineLearning.js
@@ -1,10 +1,66 @@
 import React from "react";
 import Layout from "../Layout";
+import { Link } from 'react-router-dom';
+
 
 function MachineLearning() {
   return (
     
-    <div>This is the Machine Learning page.</div>
+    <div>
+    <h1>Data Science and Machine Learning</h1>
+    <main>
+        <hr /> {/* Horizontal line separating content and references */}
+        <h2>Regression</h2>
+        <ul>
+          <li>
+            <Link to="https://changkun.de/modern-cpp/zh-cn/01-intro/"><strong>Modern C++:</strong> </Link> Comprehansive introduction for C++.
+          </li>
+        </ul>
+        <hr /> {/* Horizontal line separating content and references */}
+        <h2>Clustering</h2>
+        <ul>
+          <li>
+            <Link to="https://changkun.de/modern-cpp/zh-cn/01-intro/"><strong>Modern C++:</strong> </Link> Comprehansive introduction for C++.
+          </li>
+        </ul>
+        <hr /> {/* Horizontal line separating content and references */}
+        <h2>Descision Tree</h2>
+        <ul>
+          <li>
+            <Link to="https://changkun.de/modern-cpp/zh-cn/01-intro/"><strong>Modern C++:</strong> </Link> Comprehansive introduction for C++.
+          </li>
+        </ul>
+        <hr /> {/* Horizontal line separating content and references */}
+        <h2>Random Forest</h2>
+        <ul>
+          <li>
+            <Link to="https://changkun.de/modern-cpp/zh-cn/01-intro/"><strong>Modern C++:</strong> </Link> Comprehansive introduction for C++.
+          </li>
+        </ul>
+        <hr /> {/* Horizontal line separating content and references */}
+        <h2>Neural Network</h2>
+        <ul>
+          <li>
+            <Link to="https://changkun.de/modern-cpp/zh-cn/01-intro/"><strong>Modern C++:</strong> </Link> Comprehansive introduction for C++.
+          </li>
+        </ul>
+        <hr /> {/* Horizontal line separating content and references */}
+        <h2>Naive Bayes classifier</h2>
+        <ul>
+          <li>
+            <Link to="https://changkun.de/modern-cpp/zh-cn/01-intro/"><strong>Modern C++:</strong> </Link> Comprehansive introduction for C++.
+          </li>
+        </ul>
+        <hr /> {/* Horizontal line separating content and references */}
+        <h2>Support Vector Machine</h2>
+        <ul>
+          <li>
+            <Link to="https://changkun.de/modern-cpp/zh-cn/01-intro/"><strong>Modern C++:</strong> </Link> Comprehansive introduction for C++.
+          </li>
+        </ul>
+        
+    </main>
+    </div>
     
   );
 }
